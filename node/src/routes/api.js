c59ec79d06f0beeb8a6abe98d5cf85a2eb22b90a
isanghyeon <cpd4268@naver.com>
2022-11-30 03:16:39 +0900

    Updated; node app

--- node/src/routes/api.js
+++ node/src/routes/api.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const {communication} = require("../controller/api.js");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/sign', async function (req, res, next) {
+    res.send('respond with a resource');
 });
+
+/* GET users listing. */
+router.get('/product', async function (req, res, next) {
+    let apiData = await communication("product");
+    res.json(apiData);
+
+});
+
+/* GET users listing. */
+router.get('/shop', async function (req, res, next) {
+    res.send('respond with a resource');
+});
+
 
 module.exports = router;
